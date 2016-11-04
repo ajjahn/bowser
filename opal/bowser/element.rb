234c635fbe77d9e4f14cda9782a5aad5822f31c6
Jamie Gaskins <jamie@staq.com>
2016-11-03 22:13:44 -0400

    Add YARDocs

--- opal/bowser/element.rb
+++ opal/bowser/element.rb
@@ -2,26 +2,41 @@ require 'bowser/event_target'
 require 'bowser/file_list'
 
 module Bowser
+  # Wrap a native DOM element
   class Element
     include EventTarget
 
+    # @param native [JS] The native DOM element to wrap
     def initialize native
       @native = native
     end
 
-    def inner_dom= node
+    # Replace all child elements with the given element
+    #
+    # @param element [Bowser::Element] The Bowser element with which to replace
+    #   this element's contents
+    def inner_dom= element
       clear
-      append node
+      append element
     end
 
+    # The contents of this element as an HTML string
+    #
+    # @return [String] the HTML representation of this element's contents
     def inner_html
       `#@native.innerHTML`
     end
 
+    # Use the supplied HTML string to replace this element's contents
+    #
+    # @param html [String] the HTML with which to replace this elements contents
     def inner_html= html
       `#@native.innerHTML = html`
     end
 
+    # This element's direct child elements
+    #
+    # @return [Array<Bowser::Element>] list of this element's children
     def children
       elements = []
 
@@ -35,10 +50,17 @@ module Bowser
       elements
     end
 
+    # Determine whether this element has any contents
+    #
+    # @return [Boolean] true if the element has no children, false otherwise
     def empty?
       `#@native.children.length === 0`
     end
 
+    # Remove all contents from this element. After this call, `empty?` will
+    # return `true`.
+    #
+    # @return [Bowser::Element] self
     def clear
       if %w(input textarea).include? type
         `#@native.value = null`
@@ -51,34 +73,62 @@ module Bowser
       self
     end
 
+    # Remove the specified child element
+    #
+    # @param child [Bowser::Element] the child element to remove
     def remove_child child
       `#@native.removeChild(child.native ? child.native : child)`
     end
 
+    # This element's type. For example: "div", "span", "p"
+    #
+    # @return [String] the HTML tag name for this element
     def type
       `#@native.nodeName`.downcase
     end
 
-    def append node
-      `#@native.appendChild(node.native ? node.native : node)`
+    # Append the specified element as a child element
+    #
+    # @param element [Bowser::Element, JS] the element to insert
+    def append element
+      `#@native.appendChild(element.native ? element.native : element)`
       self
     end
 
-    # Form input methods
+    # Methods for <input /> elements
+
+    # A checkbox's checked status
+    #
+    # @return [Boolean] true if the checkbox is checked, false otherwise
     def checked?
       `!!#@native.checked`
     end
 
-    # Convenience for when you only need a single file
+    # Get the currently selected file for this input. This is only useful for
+    # file inputs without the `multiple` property set.
+    #
+    # @return [Bowser::File] the file selected by the user
     def file
       files.first
     end
 
+    # Get the currently selected files for this input. This is only useful for
+    # file inputs with the `multiple` property set.
+    #
+    # @return [Bowser::FileList] the currently selected files for this input
     def files
       FileList.new(`#@native.files`)
     end
 
-    # Fall back to native properties.
+    # Fall back to native properties. If the message sent to this element is not
+    # recognized, it checks to see if it is a property of the native element. It
+    # also checks for variations of the message name, such as:
+    #
+    #   :supported? => [:supported, :isSupported]
+    #
+    # If a property with the specified message name is found and it is a
+    # function, that function is invoked with `args`. Otherwise, the property
+    # is returned as is.
     def method_missing message, *args, &block
       camel_cased_message = message
         .gsub(/_\w/) { |match| match[1].upcase }
@@ -117,6 +167,9 @@ module Bowser
       end
     end
 
+    # The native representation of this element.
+    #
+    # @return [JS] the native element wrapped by this object.
     def to_n
       @native
     end
--- opal/bowser/http/form_data.rb
+++ opal/bowser/http/form_data.rb
@@ -1,10 +1,18 @@
 module Bowser
   module HTTP
+    # Data to be attached to a form or sent in with a Bowser::HTTP::Request
     class FormData
-      def initialize
+      # param attributes [Hash, nil] the attributes to attach
+      def initialize(attributes={})
         @native = `new FormData()`
+
+        attributes.to_h.each do |key, value|
+          append key, value
+        end
       end
 
+      # @param key [String] the name of the attribute
+      # @param value [String] the value of the attribute
       def append key, value
         data = if `!!value.native`
                  `value.native`
